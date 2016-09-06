c9838c44d3c934f924be6b38f4a8103d9702cc0b
glennqhurd <glennqhurd@gmail.com>
2016-09-06 04:13:16 +0000

    Transferred previous changes that were working to Spades.pl.  Made more changes to spades_test.pl and found more bugs when adding subroutines.

--- spades.pl
+++ spades.pl
@@ -24,51 +24,53 @@ my $spades_broken = 0;
 
 while (<>) {
   next if $. == 1;
-  my @current_line = split ' ', $_;
+  $_ =~ /^\s*\d+\s+(\S+)\s+\d+\s+(\S+)\s+\d+\s+(\S+)\s+\d+\s+(\S+)\s*$/;
   my $column_count = 1;
-  foreach (@current_line){
-    if (($_ =~ /^[0-9]+$/) and ($column_count % 2 == 0)){
-      foreach my $suit (@suits) {
-        my $composite;
-        foreach my $card_number (2..10){
-          $composite = $card_number.$suit;
-          $deck{$composite} = $card_number;
-        }
-        $composite = "A".$suit;
-        $deck{$composite} = 14;
+  my $var1 = $1;
+  my $var2 = $2;
+  my $var3 = $3;
+  my $var4 = $4;
+  if (($var1 =~ /^\d+$/)|($var2 =~ /^\d+$/)|($var3 =~ /^\d+$/)|($var4 =~ /^\d+$/)){
+    foreach my $suit (@suits) {
+      my $composite;
+      foreach my $card_number (2..10){
+        $composite = $card_number.$suit;
+        $deck{$composite} = $card_number;
+      }
+      $composite = "A".$suit;
+      $deck{$composite} = 14;
 
-        $composite = "K".$suit;
-        $deck{$composite} = 13;
+      $composite = "K".$suit;
+      $deck{$composite} = 13;
 
-        $composite = "Q".$suit;
-        $deck{$composite} = 12;
+      $composite = "Q".$suit;
+      $deck{$composite} = 12;
 
-        $composite = "J".$suit;
-        $deck{$composite} = 11;
-      }
-
-      push(@bids, $_);
-      if ($column_count / 2 == 1){
-        $player_bids1 += $_;
-      }
-      if ($column_count / 2 == 2){
-        $player_bids2 += $_;
-      }
-      if ($column_count / 2 == 3){
-        $player_bids3 += $_;
-      }
-      if ($column_count / 2 == 4){
-        $player_bids4 += $_;
-      }
+      $composite = "J".$suit;
+      $deck{$composite} = 11;
     }
-    elsif (($column_count % 2 == 0) and ($_ ne "--")){
-      delete $deck{$_};
-      if (($spades_broken == 0) and ($_ =~ /S/)){
-        $spades_broken = 1;
-      }
+
+    push(@bids, $_);
+    if ($var1 =~ /^\d+$/){
+      $player_bids1 += $var1;
     }
-    $column_count += 1;
+    if ($var2 =~ /^\d+$/){
+      $player_bids2 += $var2;
+    }
+    if ($var3 =~ /^\d+$/){
+      $player_bids3 += $var3;
+    }
+    if ($var4 =~ /^\d+$/){
+      $player_bids4 += $var4;
+    }
   }
+  elsif (($column_count % 2 == 0) and ($_ ne "--")){
+    delete $deck{$_};
+    if (($spades_broken == 0) and ($_ =~ /S/)){
+      $spades_broken = 1;
+    }
+  }
+  $column_count += 1;
 }
 
 print "Player 1:".$player_bids1."\n";
--- spades_test.pl
+++ spades_test.pl
@@ -10,27 +10,14 @@ my $player_bids3;
 my $player_bids4;
 my %deck;
 my @suits = qw(D C H S);
-
-@ARGV == 1 or die "Only accepts 1 file input.\n";
-
-open( my $file, $ARGV[0]);
-
-my $line = <$file>;
-
-my @player_names = split ' ', $line;
-
-my @first_spades_move;
 my $spades_broken = 0;
+my $var1;
+my $var2;
+my $var3;
+my $var4;
 
-while (<>) {
-  next if $. == 1;
-  $_ =~ /^\s*\d+\s+(\S+)\s+\d+\s+(\S+)\s+\d+\s+(\S+)\s+\d+\s+(\S+)\s*$/;
-  my $column_count = 1;
-  my $var1 = $1;
-  my $var2 = $2;
-  my $var3 = $3;
-  my $var4 = $4;
-  if (($var1 =~ /^\d+$/)|($var2 =~ /^\d+$/)|($var3 =~ /^\d+$/)|($var4 =~ /^\d+$/)){
+sub create_deck {
+  if (($var1 =~ /^\d+$/)|($var2 =~ /^\d+$/)|($var3 =~ /^\d+$/)|($var4 =~ /^\d+$$
     foreach my $suit (@suits) {
       my $composite;
       foreach my $card_number (2..10){
@@ -49,8 +36,11 @@ while (<>) {
       $composite = "J".$suit;
       $deck{$composite} = 11;
     }
+  }
+}
 
-    push(@bids, $_);
+sub tally_bids {
+  if (($var1 =~ /^\d+$/)|($var2 =~ /^\d+$/)|($var3 =~ /^\d+$/)|($var4 =~ /^\d+$$){
     if ($var1 =~ /^\d+$/){
       $player_bids1 += $var1;
     }
@@ -64,6 +54,27 @@ while (<>) {
       $player_bids4 += $var4;
     }
   }
+}
+
+@ARGV == 1 or die "Only accepts 1 file input.\n";
+
+open( my $file, $ARGV[0]);
+
+my $line = <$file>;
+
+my @player_names = split ' ', $line;
+
+while (<>) {
+  next if $. == 1;
+  $_ =~ /^\s*\d+\s+(\S+)\s+\d+\s+(\S+)\s+\d+\s+(\S+)\s+\d+\s+(\S+)\s*$/;
+  my $column_count = 1;
+  $var1 = $1;
+  $var2 = $2;
+  $var3 = $3;
+  $var4 = $4;
+  &create_deck;
+  &tally_bids;
+
   elsif (($column_count % 2 == 0) and ($_ ne "--")){
     delete $deck{$_};
     if (($spades_broken == 0) and ($_ =~ /S/)){
